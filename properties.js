241bab9611632bd029fa42634d68b2fdc4821da7
Bryan Agustine C. Cabansay <bryancabansay@gmail.com>
2020-06-22 21:03:11 +0800

    Remove property values

--- properties.js
+++ properties.js
@@ -4,10 +4,10 @@ const port = 2000;
 
 // Change these values according to environment (prod / dev)
 const network = 'ropsten' // default is mainnet;
-const network_provider = 'wss://ropsten.infura.io/ws/v3/13138ea84bd84a0cbd95f88a7e12c054';
-const contract_address = '0x89031D05bf46458d5E907AFAae91584e19C50FB9';
-const roks_src_address = '0x0d606668Fcf49942F19Fdfd5A3aFB01eA5163EF5';
-const roks_src_priv_key = 'AA7BE917A05F15FC70AA4ACBC3A3120372FD560A6D643BF09A614251C75DB9A7';
+const network_provider = 'network-provider here e.g. infura';
+const contract_address = 'change to the smart contract address';
+const roks_src_address = 'address of the source of ROKS';
+const roks_src_priv_key = 'private key of the source of ROKS';
 
 // Change the gas limit as per requirement
 const gas_limit = '900000';
